de48823807f83169ed549fef8b3a3f66188847c0
배성완 <baesee@kakao.com>
2023-07-04 16:44:54 +0900

    input textarea handleChangeState 지정 react-copy-to-clipboard 라이브러리 사용 버튼 클릭시 글 복사

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import TILEditor from "./TILEditor";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div>
+    <Container>
       <h2>To Day I Learned</h2>
       <TILEditor />
-    </div>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  margin: 0 auto;
+  text-align: center;
+`;
 export default App;
--- src/TILEditor.jsx
+++ src/TILEditor.jsx
@@ -1,38 +1,69 @@
 import React, { useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+import styled from "styled-components";
 
 function TILEditor() {
   const [state, setState] = useState({
-    author: "",
+    title: "",
     content: "",
   });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
       <h2>오늘의 TIL</h2>
-      <div>
-        <input
+      <TitleBox>
+        <Title
+          name="title"
           type="text"
-          value={state.author}
-          onChange={(e) => {
-            setState({
-              author: e.target.value,
-              ...state,
-            });
-          }}
+          value={state.title}
+          onChange={handleChangeState}
         />
+      </TitleBox>
+      <div>
+        <Content
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        ></Content>
       </div>
       <div>
-        <textarea
-          value={state.content}
-          onChange={(e) => {
-            setState({
-              ...state,
-              content: e.target.value,
-            });
+        <CopyToClipboard
+          text={"제목 " + state.author + "\n" + "내용 " + state.content}
+          onCopy={() => {
+            alert("클립보드에 복사되었습니다");
           }}
-        ></textarea>
+        >
+          <Button>내용 복사</Button>
+        </CopyToClipboard>
       </div>
     </div>
   );
 }
-
+const TitleBox = styled.div`
+  margin-bottom: 20px;
+`;
+const Title = styled.input`
+  width: 300px;
+  height: 30px;
+  font-size: 20px;
+  border: none;
+  border-bottom: 1px solid #000;
+`;
+const Content = styled.textarea`
+  width: 650px;
+  height: 650px;
+  font-size: 20px;
+`;
+const Button = styled.button`
+  margin-top: 20px;
+  width: 100px;
+  height: 30px;
+`;
 export default TILEditor;
